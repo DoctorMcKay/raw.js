c86c63798857eacf4c1653b83c2ecafe1f2c062b
Doctor_McKay <Doctor_McKay@localhost>
2014-04-22 12:44:23 -0400

    Convert modlog method to a simple request

--- api/modlog.js
+++ api/modlog.js
@@ -1,8 +1,5 @@
 var reddit = global.reddit;
 
-reddit.prototype.modlog = function(r, callback) {
-	var self = this;
-	this._apiRequest("log", {"path": "/r/" + r + "/about"}, function(err, response, body) {
-		self._listing(err, body, callback);
-	});
-};
+// reddit._addListingRequest = function(name, endpoint, path)
+
+reddit._addListingRequest("modlog", "log.json", "/about");
