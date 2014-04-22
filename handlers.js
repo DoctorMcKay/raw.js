4cfab4692da9663aa5b85cef13f9c796814bbb64
Alexander Corn <DoctorMcKay@users.noreply.github.com>
2014-04-21 20:30:34 -0400

    Added handlers

---
+++ handlers.js
@@ -0,0 +1,52 @@
+var reddit = global.reddit;
+
+reddit.prototype._modifySingleItem = function(err, body, callback) {
+	if(!callback) {
+		return;
+	}
+	
+	if(err) {
+		callback(err);
+		return;
+	}
+	
+	try {
+		var json = JSON.parse(body);
+		console.log(json);
+		if(json.error) {
+			callback(json.error);
+		} else if(json.json.errors.length == 1) {
+			callback(json.json.errors[0]);
+		} else if(json.json.errors.length > 1) {
+			callback(json.json.errors);
+		} else {
+			callback(null, json.json.data.things[0]);
+		}
+	} catch(e) {
+		callback("reddit API returned invalid response: " + e);
+	}
+}
+
+reddit.prototype._noResponse = function(err, body, callback) {
+	if(!callback) {
+		return;
+	}
+	
+	if(err) {
+		callback(err);
+		return;
+	}
+	
+	try {
+		var json = JSON.parse(body);
+		if(json.error) {
+			callback(json.error);
+			return;
+		}
+	} catch(e) {
+		callback("reddit API returned invalid response: " + e);
+		return;
+	}
+	
+	callback();
+};
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var request = require('request');
 var Exception = require('./exception.js');
 
 module.exports = reddit;
+global.reddit = reddit;
 
 require('util').inherits(reddit, require('events').EventEmitter);
 
