4eb33d79919e3a03bef045646cde642bc5b2dcea
Alexander Corn <DoctorMcKay@users.noreply.github.com>
2014-04-21 23:01:55 -0400

    Added getPrefs and trophies

--- api/identity.js
+++ api/identity.js
@@ -6,3 +6,32 @@ reddit.prototype.me = function(callback) {
 		self._rawJSON(err, body, callback);
 	});
 };
+
+reddit.prototype.getPrefs = function(prefs, callback) {
+	if(typeof prefs == 'function') {
+		callback = prefs;
+		prefs = [];
+	}
+	
+	var self = this;
+	this._apiRequest("prefs", {"path": "/api/v1/me", "qs": {"fields": prefs.join(',')}}, function(err, response, body) {
+		self._rawJSON(err, body, callback);
+	});
+};
+
+reddit.prototype.trophies = function(callback) {
+	var self = this;
+	this._apiRequest("trophies", {"path": "/api/v1/me"}, function(err, response, body) {
+		try {
+			var json = JSON.parse(body);
+			if(json.error) {
+				callback(json.error);
+				return;
+			}
+			
+			callback(null, json.data.trophies);
+		} catch(e) {
+			callback("reddit API returned invalid response: " + e);
+		}
+	});
+};
