e15e8ae9c235d10e5f4dee1a1de26ec17ee7f3af
Alexander Corn <DoctorMcKay@users.noreply.github.com>
2014-04-22 12:44:13 -0400

    Added simple request for Listings

--- simplerequests.js
+++ simplerequests.js
@@ -26,3 +26,47 @@ reddit._addSimpleRequest = function(name, endpoint, method, args, constArgs, cal
 		});
 	};
 };
+
+reddit._addListingRequest = function(name, endpoint, path) {
+	reddit.prototype[name] = function() {
+		var options;
+		var callback;
+		if(arguments.length == 0) {
+			options = null;
+			callback = null;
+		} else if(arguments.length == 1 && typeof arguments[0] == 'object') {
+			options = arguments[0];
+			callback = null;
+		} else if(arguments.length == 1 && typeof arguments[0] == 'function') {
+			options = null;
+			callback = arguments[0];
+		} else {
+			options = arguments[0];
+			callback = arguments[1];
+		}
+		
+		var requestPath = '';
+		if(options && options.r) {
+			requestPath = "/r/" + options.r;
+		}
+		
+		if(path) {
+			requestPath += path;
+		}
+		
+		var qs;
+		if(options.after || options.before || options.limit || options.count || options.all) {
+			qs = {};
+			qs.after = options.after;
+			qs.before = options.before;
+			qs.limit = options.limit;
+			qs.count = options.count;
+			qs.all = (options.all) ? "all" : undefined;
+		}
+		
+		var self = this;
+		this._apiRequest(endpoint, {"path": requestPath, "qs": qs}, function(err, response, body) {
+			self._listing(err, body, callback);
+		});
+	};
+};
