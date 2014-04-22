068d6a3138cc242a609ecf423fcb2c3dd7cd07f6
Alexander Corn <DoctorMcKay@users.noreply.github.com>
2014-04-22 01:11:24 -0400

    Renamed delete method to del

--- api/edit.js
+++ api/edit.js
@@ -2,6 +2,6 @@ var reddit = global.reddit;
 
 // reddit._addSimpleRequest = function(name, endpoint, method, args, constArgs, callback)
 
-reddit._addSimpleRequest("delete", "del", "POST", ["id"], null, "_noResponse");
+reddit._addSimpleRequest("del", "del", "POST", ["id"], null, "_noResponse");
 reddit._addSimpleRequest("edit", "editusertext", "POST", ["thing", "text"], {"api_type": "json"}, "_modifySingleItem");
 reddit._addSimpleRequest("inboxReplies", "sendreplies", "POST", ["thing", "state"], null, "_noResponse");
