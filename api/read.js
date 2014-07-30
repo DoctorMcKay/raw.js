cb52a0971c874f196e9c531eb2786e49feaf9511
Alexander Corn <DoctorMcKay@users.noreply.github.com>
2014-07-29 22:23:21 -0400

    Added liveDiscussions method

--- api/read.js
+++ api/read.js
@@ -153,3 +153,4 @@ reddit._addListingRequest("unmoderated", "unmoderated.json", "/about", ["only"])
 reddit._addListingRequest("liveUpdates", ".json");
 reddit._addListingRequest("liveThreadInfo", "about.json");
 reddit._addListingRequest("liveThreadContributors", "contributors.json");
+reddit._addListingRequest("liveDiscussions", "discussions.json");
