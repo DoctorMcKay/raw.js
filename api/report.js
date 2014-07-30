efb78010c35ab6b54a401e6da839d8259cc75eef
Alexander Corn <mckay@doctormckay.com>
2014-07-29 23:07:26 -0400

    Added reportLiveThread method

--- api/report.js
+++ api/report.js
@@ -5,3 +5,4 @@ var reddit = global.reddit;
 reddit._addSimpleRequest("hide", "hide", "POST", ["id"], null, "_noResponse");
 reddit._addSimpleRequest("unhide", "unhide", "POST", ["id"], null, "_noResponse");
 reddit._addSimpleRequest("report", "report", "POST", ["id"], null, "_noResponse");
+reddit._addLiveRequest("reportLiveThread", "report", "POST", ["type"], {"api_type": "json"}, "_multipleErrors"); // _multipleErrors is just a guess since I didn't want to actually report a thread to test this
