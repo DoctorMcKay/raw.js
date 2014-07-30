8daee317b66099c3812ab43fe7d8b2a2803a6fec
Alexander Corn <DoctorMcKay@users.noreply.github.com>
2014-07-29 23:16:40 -0400

    Added changeLiveContributorPermissions and changeLiveContributorInvitePermissions methods

--- api/livemanage.js
+++ api/livemanage.js
@@ -40,3 +40,67 @@ reddit._addLiveRequest("editLiveThread", "edit", "POST", ["title", "description"
 reddit._addLiveRequest("leaveLiveContributor", "leave_contributor", "POST", [], {"api_type": "json"}, "_multipleErrors");
 reddit._addLiveRequest("removeLiveContributor", "rm_contributor", "POST", ["id"], {"api_type": "json"}, "_multipleErrors");
 reddit._addLiveRequest("removeLiveContributorInvite", "rm_contributor_invite", "POST", ["id"], {"api_type": "json"}, "_multipleErrors");
+
+reddit.prototype.changeLiveContributorPermissions = function(threadID, username, permissions, callback) {
+	if(typeof permissions == 'function') {
+		callback = permissions;
+		permissions = null;
+	}
+	
+	var permissionString = "+all";
+	var possiblePermissions = ["close", "edit", "manage", "settings", "update"];
+	
+	if(permissions) {
+		permissionString = '-all';
+		for(var i = 0; i < possiblePermissions.length; i++) {
+			permissionString += ',';
+			if(permissions.indexOf(possiblePermissions[i]) != -1) {
+				permissionString += '+' + possiblePermissions[i];
+			} else {
+				permissionString += '-' + possiblePermissions[i];
+			}
+		}
+	}
+	
+	var self = this;
+	this._apiRequest("set_contributor_permissions", {"path": "/api/live/" + threadID, "method": "POST", "form": {
+		"api_type": "json",
+		"name": username,
+		"permissions": permissionString,
+		"type": "liveupdate_contributor"
+	}}, function(err, response, body) {
+		self._multipleErrors(err, body, callback);
+	});
+};
+
+reddit.prototype.changeLiveContributorInvitePermissions = function(threadID, username, permissions, callback) {
+	if(typeof permissions == 'function') {
+		callback = permissions;
+		permissions = null;
+	}
+	
+	var permissionString = "+all";
+	var possiblePermissions = ["close", "edit", "manage", "settings", "update"];
+	
+	if(permissions) {
+		permissionString = '-all';
+		for(var i = 0; i < possiblePermissions.length; i++) {
+			permissionString += ',';
+			if(permissions.indexOf(possiblePermissions[i]) != -1) {
+				permissionString += '+' + possiblePermissions[i];
+			} else {
+				permissionString += '-' + possiblePermissions[i];
+			}
+		}
+	}
+	
+	var self = this;
+	this._apiRequest("set_contributor_permissions", {"path": "/api/live/" + threadID, "method": "POST", "form": {
+		"api_type": "json",
+		"name": username,
+		"permissions": permissionString,
+		"type": "liveupdate_contributor_invite"
+	}}, function(err, response, body) {
+		self._multipleErrors(err, body, callback);
+	});
+};
