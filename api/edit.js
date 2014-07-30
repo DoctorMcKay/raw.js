819b0867f7549f794a7c2239fad2bf7db6357c6d
Alexander Corn <DoctorMcKay@users.noreply.github.com>
2014-07-29 22:08:16 -0400

    Added liveStrike method

--- api/edit.js
+++ api/edit.js
@@ -6,3 +6,4 @@ reddit._addSimpleRequest("del", "del", "POST", ["id"], null, "_noResponse");
 reddit._addSimpleRequest("edit", "editusertext", "POST", ["thing", "text"], {"api_type": "json"}, "_modifySingleItem");
 reddit._addSimpleRequest("inboxReplies", "sendreplies", "POST", ["thing", "state"], null, "_noResponse");
 reddit._addLiveRequest("liveDelete", "delete_update", "POST", ["id"], {"api_type": "json"}, "_multipleErrors");
+reddit._addLiveRequest("liveStrike", "strike_update", "POST", ["id"], {"api_type": "json"}, "_multipleErrors");
