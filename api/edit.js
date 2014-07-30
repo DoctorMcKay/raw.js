e57fe01a49a7716df98bec390b61be776d298483
Alexander Corn <mckay@doctormckay.com>
2014-07-29 22:01:10 -0400

    Added liveDelete method

--- api/edit.js
+++ api/edit.js
@@ -5,3 +5,4 @@ var reddit = global.reddit;
 reddit._addSimpleRequest("del", "del", "POST", ["id"], null, "_noResponse");
 reddit._addSimpleRequest("edit", "editusertext", "POST", ["thing", "text"], {"api_type": "json"}, "_modifySingleItem");
 reddit._addSimpleRequest("inboxReplies", "sendreplies", "POST", ["thing", "state"], null, "_noResponse");
+reddit._addLiveRequest("liveDelete", "delete_update", "POST", ["id"], {"api_type": "json"}, "_multipleErrors");
