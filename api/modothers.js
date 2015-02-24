cb7ba83abc04e52a9a1cb74c025b52eab2705dbe
Alexander Corn <mckay@doctormckay.com>
2015-02-23 23:53:32 -0500

    Added inviteModerator method

--- api/modothers.js
+++ api/modothers.js
@@ -18,30 +18,50 @@ reddit.prototype.changeModeratorPermissions = function(subreddit, username, perm
 	changePerms(this, subreddit, username, permissions, "moderator", callback);
 };
 
-function changePerms(self, sr, user, perms, type, callback) {
-	var permissionString = "+all";
-	var possiblePermissions = ["access", "config", "flair", "mail", "posts", "wiki"];
-	
-	if(perms) {
-		permissionString = '-all';
-		possiblePermissions.forEach(function(perm) {
-			permissionString += ',';
-			if(perms.indexOf(perm) != -1) {
-				permissionString += '+';
-			} else {
-				permissionString += '-';
-			}
-			
-			permissionString += perm;
-		});
+function resolvePermissions(permissions) {
+	if(!permissions) {
+		return "+all";
 	}
 	
+	var permissionString = '-all';
+	["access", "config", "flair", "mail", "posts", "wiki"].forEach(function(perm) {
+		permissionString += ',';
+		if(permissions.indexOf(perm) != -1) {
+			permissionString += '+';
+		} else {
+			permissionString += '-';
+		}
+		
+		permissionString += perm;
+	});
+	
+	return permissionString;
+}
+
+function changePerms(self, sr, user, perms, type, callback) {
 	self._apiRequest("setpermissions", {"path": "/r/" + sr + "/api", "method": "POST", "form": {
 		"api_type": "json",
 		"name": user,
-		"permissions": permissionString,
+		"permissions": resolvePermissions(perms),
 		"type": type
 	}}, function(err, response, body) {
 		self._multipleErrors(err, body, callback)
 	});
 }
+
+reddit.prototype.inviteModerator = function(subreddit, username, permissions, callback) {
+	if(typeof permissions === 'function') {
+		callback = permissions;
+		permissions = null;
+	}
+	
+	var self = this;
+	this._apiRequest("friend", {"path": "/r/" + subreddit + "/api", "method": "POST", "form": {
+		"api_type": "json",
+		"name": username,
+		"permissions": resolvePermissions(permissions),
+		"type": "moderator_invite"
+	}}, function(err, response, body) {
+		self._multipleErrors(err, body, callback);
+	});
+};
